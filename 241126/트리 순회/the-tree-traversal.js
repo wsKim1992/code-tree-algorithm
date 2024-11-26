6daa9eb49b1fd3b70e19dcf7a2a68bf4801f9f3f
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-11-26 15:58:45 +0900

    Title: 트리 순회, Time: 189ms, Memory: 15MB, Status: Passed - CodeTree

--- 241126/트리 순회/the-tree-traversal.js
+++ 241126/트리 순회/the-tree-traversal.js
@@ -1,56 +1,39 @@
 const fs = require("fs")
 const process = require('process')
-const firstDFS = (tree,idx)=>{
-    if(tree[idx]==='.')return;
-    process.stdout.write(tree[idx]);
-    firstDFS(tree,idx*2)
-    firstDFS(tree,idx*2+1)
+const firstDFS = (leftTree,rightTree,strIdx)=>{
+    process.stdout.write(strIdx);
+    leftTree[strIdx]!=='.'&&firstDFS(leftTree,rightTree,leftTree[strIdx]);
+    rightTree[strIdx]!=='.'&&firstDFS(leftTree,rightTree,rightTree[strIdx]);
 }
-const secondDFS = (tree,idx)=>{
-    if(tree[idx]==='.')return ;
-    secondDFS(tree,idx*2);
-    process.stdout.write(tree[idx]);
-    secondDFS(tree,idx*2+1);
+const secondDFS = (leftTree,rightTree,strIdx)=>{
+    leftTree[strIdx]!=='.'&&secondDFS(leftTree,rightTree,leftTree[strIdx]);
+    process.stdout.write(strIdx);
+    rightTree[strIdx]!=='.'&&secondDFS(leftTree,rightTree,rightTree[strIdx]);
 }
 
-const thirdDFS = (tree,idx)=>{
-    if(tree[idx]==='.')return;
-    thirdDFS(tree,idx*2);
-    thirdDFS(tree,idx*2+1);
-    process.stdout.write(tree[idx]);
+const thirdDFS = (leftTree,rightTree,strIdx)=>{
+    leftTree[strIdx]!=='.'&&thirdDFS(leftTree,rightTree,leftTree[strIdx]);
+    rightTree[strIdx]!=='.'&&thirdDFS(leftTree,rightTree,rightTree[strIdx]);
+    process.stdout.write(strIdx);
 }
 
 const main = ()=>{
     const inputLines = fs.readFileSync(0).toString().split("\n");
     const n = Number(inputLines[0]);
-    const tree = Array.from({length:Math.pow(2,n+1)}).map(()=>'.');
-    const location = {};
-    inputLines.slice(1).forEach((line,idx)=>{
+    const leftTree ={};
+    const rightTree = {};
+    inputLines.slice(1).forEach(line=>{
         if(line!==''){
             const [p,l,r] = line.split(" ")
-            if(location[p]===undefined){
-                location[p]=idx+1;
-            }
-            tree[location[p]]=p;
-            if(l!=='.'){
-                if(location[l]===undefined){
-                    location[l]=location[p]*2
-                }
-                tree[location[l]]=l;
-            }
-            if(r!=='.'){
-                if(location[r]===undefined){
-                    location[r]=location[p]*2+1;
-                }
-                tree[location[r]]=r;
-            }
+            leftTree[p]=l;
+            rightTree[p]=r;
         }
     })
-    firstDFS(tree,1);
-    console.log()
-    secondDFS(tree,1);
-    console.log()
-    thirdDFS(tree,1);
+    firstDFS(leftTree,rightTree,'A');
+    console.log();
+    secondDFS(leftTree,rightTree,'A');
+    console.log();
+    thirdDFS(leftTree,rightTree,'A');
 }
 
 main();
