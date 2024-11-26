e136744d05de4e89f4a62d354c50c2f263f4f215
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-11-26 18:29:21 +0900

    Title: 트리 경유, Time: 1153ms, Memory: 45MB, Status: Wrong Answer - CodeTree

---
+++ 241126/트리 경유/a-way-through-the-tree.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const search = (tree,idx,start)=>{
+    if(tree[idx]===1)return {isPossible:false,idx}
+    if(idx===1){
+        tree[start]=1;
+        return {isPossible:true,idx}
+    }
+    return search(tree,Math.floor(idx/2),start)
+}
+
+const main = ()=>{
+    const inputlines = fs.readFileSync(0).toString().split("\n");
+    const [n,q] = inputlines[0].split(" ").map(Number);
+    const tree = Array.from({length:n+1}).map(()=>0)
+    const arr = []
+    inputlines.slice(1).forEach(line=>{
+        if(line!==''){
+            arr[arr.length]=Number(line);
+        }
+    })
+    arr.forEach(e=>{
+        const {isPossible,idx} = search(tree,e,e);
+        console.log(isPossible?Number(!isPossible):idx);
+    })
+}
+
+main();
