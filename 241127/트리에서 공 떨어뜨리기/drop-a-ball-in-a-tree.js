17b1d6607be2ee107fd49afe36f3b7149575e4a3
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-11-27 20:23:56 +0900

    Title: 트리에서 공 떨어뜨리기, Time: 395ms, Memory: 43MB, Status: Wrong Answer - CodeTree

--- 241127/트리에서 공 떨어뜨리기/drop-a-ball-in-a-tree.js
+++ 241127/트리에서 공 떨어뜨리기/drop-a-ball-in-a-tree.js
@@ -1,37 +1,37 @@
 const fs = require("fs");
-
-const getIndex = (tree,idx,k)=>{
-    const leftVal = tree[idx*2];
-    const rightVal = tree[idx*2+1];
+const MAX_N=200000;
+const getIndex = (leftTree,rightTree,idx,k)=>{
+    const leftVal = leftTree[idx];
+    const rightVal = rightTree[idx*2+1];
     const isHaveTwoChild = leftVal!==-1&&rightVal!==-1;
     const isHaveOneChild = leftVal!==-1||rightVal!==-1;
     if(isHaveTwoChild){
         const isOdd = k%2===1;
-        return isOdd?getIndex(tree,idx*2,(Math.floor((k+1)/2))):getIndex(tree,2*idx+1,Math.floor(k/2))
+        return isOdd?getIndex(leftTree,rightTree,leftTree[idx],(Math.floor((k+1)/2))):getIndex(leftTree,rightTree,rightTree[idx],Math.floor(k/2))
     }else if(isHaveOneChild){
-        return leftVal!==-1?getIndex(tree,idx*2,k):getIndex(tree,2*idx+1,k);
+        return leftVal!==-1?getIndex(leftTree,rightTree,leftTree[idx],k):getIndex(leftTree,rightTree,rightTree[idx],k);
     }else{
         return idx;
     }
-    
-
 }
 
 const main = ()=>{
     const inputLines = fs.readFileSync(0).toString().split("\n");
     const n = Number(inputLines[0]);
     const tree = Array.from({length:n+1}).map(()=>-1);
+    const leftTree = Array.from({length:MAX_N+1}).map(()=>-1);
+    const rightTree = Array.from({length:MAX_N+1}).map(()=>-1)
     inputLines.slice(1,n+1).forEach((line,idx)=>{
         if(line!==''){
             const [l,r] = line.split(" ").map(Number);
             const newIdx = idx+1;
-            tree[2*newIdx]=l;
-            tree[2*newIdx+1]=r;
+            leftTree[newIdx]=l;
+            rightTree[newIdx]=r;
         }
     })
     const k = Number(inputLines[inputLines.length-1]);
-    const ans = getIndex(tree,1,k);
-    console.log(tree[ans]);
+    const ans = getIndex(leftTree,rightTree,1,k);
+    console.log(ans);
 }
 
 main();
