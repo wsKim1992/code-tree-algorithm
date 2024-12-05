b341bfa93855f70b93fad0e4107d8b69a4099f15
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-12-05 18:28:39 +0900

    Title: 트리 전달, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241205/트리 전달/tree-forwarding.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX_N 100000
+
+using namespace std;
+
+int parents[MAX_N+1];
+vector<int>edges[MAX_N+1];
+int d[MAX_N+1];
+int n;
+int m;
+int root=-1;
+void DFS(int x){
+    if(parents[x]!=-1){
+        d[x]+=d[parents[i]];
+    }
+    for(int i=0;i<edges[x].size();i++){
+        int y = edges[x][i];
+        DFS(y);
+    }
+}
+
+int main() {
+    cin>>n>>m;
+    for(int i =1;i<=n;i++){
+        cin>>parents[i];
+        d[i]=0;
+        if(parents[i]!=-1){
+            edges[parents[i]].push_back(i);
+        }
+        if(parents[i]==-1){
+            root=i;
+        }
+    }
+    for(int i =0;i<m;i++){
+        int c,w;
+        cin>>c>>w;
+        d[c]+=w;
+    }
+    DFS(root)
+    for(int i=1;i<=n;i++){
+        cout<<d[i]<<" ";
+    }
+    return 0;
+}
