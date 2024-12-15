65446e78855b6ed2798a8238c08ee14626ab04cf
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-12-15 13:35:52 +0900

    Title: 최소 스패닝 트리 4, Time: 91ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 241215/최소 스패닝 트리 4/minimum-spanning-tree-4.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <tuple>
+#define MAX_N 10000
+#define MAX_M 100000
+using namespace std;
+
+int n,m;
+
+tuple<int,int,int>edges[MAX_M+1];
+int uf[MAX_N+1];
+char type[MAX_N+1];
+
+int find(int x){
+    if(uf[x]==x)return x;
+    return uf[x]=find(uf[x]);
+}
+
+void uni(int x,int y){
+    int rootX = find(x);
+    int rootY = find(y);
+    uf[rootX]=rootY;
+}
+
+int main() {
+    cin>>n>>m;
+    for(int i = 1;i<=n;i++){
+        cin>>type[i];
+        uf[i]=i;
+    }
+    for(int i =0;i<m;i++){
+        int x,y,z;
+        cin>>x>>y>>z;
+        if(type[x]!=type[y]){
+            edges[i]=make_tuple(z,x,y);
+        }
+    }
+    int ans=0;
+    sort(edges+1,edges+m+1);
+    for(int i=0;i<m;i++){
+        int x,y,cost;
+        tie(cost,x,y)=edges[i];
+        int rootX = find(x);
+        int rootY = find(y);
+        if(rootX!=rootY){
+            uni(x,y);
+            ans+=cost;
+        }
+    }
+    if(ans>0){
+        cout<<ans<<endl;
+    }else{
+        cout<<-1<<endl;
+    }
+    return 0;
+}
