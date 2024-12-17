8ea495ce308195e4ec07f43a9c921b785a97656b
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-12-17 20:01:58 +0900

    Title: 가장 많은 정점과 연결, Time: 37ms, Memory: 2MB, Status: Passed - CodeTree

--- 241217/가장 많은 정점과 연결/connect-with-the-most-vertices.cpp
+++ 241217/가장 많은 정점과 연결/connect-with-the-most-vertices.cpp
@@ -1,77 +1,82 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <tuple>
-#include <utility>
-#define MAX_N 100000
+
+#define MAX_N 100002
+#define MAX_M 100002
 
 using namespace std;
 
-int n,m,k;
-vector<tuple<int,int,int>>edges;
-int uf[MAX_N+1];
-int num[MAX_N+1];
-bool visited[MAX_N+1];
-int numOfCluster=0;
+// 변수 선언
+int n, m, k;
 
-int find(int x){
-    if(x==uf[x])return uf[x];
-    return uf[x]=find(uf[x]);
+// 각 그룹의 최소비용을 관리합니다.
+int min_cost[MAX_N+ 1];
+
+bool visited[MAX_N + 1];
+
+vector<int> cost_list;
+
+int uf[MAX_N + 1];
+
+int Find(int x) {
+    if(uf[x] == x)
+        return x;
+    return uf[x] = Find(uf[x]);
 }
 
-void Union(int x, int y){
-    int rootX = find(x);
-    int rootY = find(y);
-    uf[rootX]=rootY;
+void Union(int x, int y) {
+    int X = Find(x);
+    int Y = Find(y);
+    uf[X] = Y;
+
+    // 각 그룹의 최소비용을 관리합니다.
+    min_cost[Y] = min(min_cost[Y], min_cost[X]);
 }
 
 int main() {
-    int ans =0;
-    cin>>n>>m>>k;
-    for(int i=1;i<=n;i++){
-        uf[i]=i;
+    // 입력:
+    cin >> n >> m >> k;
+
+    // min_cost의 초기값이 주어집니다.
+    for(int i = 1; i <= n; i++)
+        cin >> min_cost[i];
+
+    // uf 값을 초기값을 적어줍니다.
+    for(int i = 1; i <= n; i++)
+        uf[i] = i;
+
+    for(int i = 1; i <= m; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        // 이미 연결되어 있으므로 union을 진행합니다.
+        Union(x, y);
     }
-    for(int i=1;i<=n;i++){
-        cin>>num[i];
+
+    // 각 그룹들에 대해 min_cost값을 모아줍니다.
+    for(int i = 1; i <= n; i++) {
+        int I = Find(i);
+
+        // 중복되는 그룹은 패스합니다.
+        if(visited[I])
+            continue;
+        
+        visited[I] = true;
+        cost_list.push_back(min_cost[I]);
     }
-    for(int i=0;i<m;i++){
-        int x,y;
-        cin>>x>>y;
-        if(find(x)!=find(y)){
-            Union(x,y);
-            //visited[x][y]=true;
-        }
-    }
-    for(int i=1;i<=n;i++){
-        for(int j=i+i;j<=n;j++){
-            int rootX = find(i);
-            int rootY = find(j);
-            if(rootX!=rootY){
-                if(!visited[rootX]){
-                    visited[rootX]=true;
-                    numOfCluster+=1;
-                }
-                if(!visited[rootY]){
-                    visited[rootY]=true;
-                    numOfCluster+=1;
-                }
-                edges.push_back(make_tuple(num[i]+num[j],i,j));
-            }
-        }
-    }
-    sort(edges.begin(),edges.end());
-    for(int i=0;i<(int)edges.size();i++){
-        int cost,x,y;
-        tie(cost,x,y)=edges[i];
-        if(find(x)!=find(y)&&numOfCluster-1>0){
-            Union(x,y);
-            numOfCluster-=1;
-            ans+=cost;
-        }
-    }
-    if(ans<=k){cout<<ans<<endl;}
-    else{
-        cout<<"NO"<<endl;
-    }
+
+    // min_cost를 오름차순으로 정렬합니다.
+    sort(cost_list.begin(), cost_list.end());
+
+    // 가장 작은 min_cost 쪽과 간선을 연결하는 것이 최선입니다.
+    int ans = 0;
+    for(int i = 1; i < (int) cost_list.size(); i++)
+        ans += cost_list[0] + cost_list[i];
+
+    if(ans <= k)
+        cout << ans;
+    else
+        cout << "NO";
     return 0;
 }
