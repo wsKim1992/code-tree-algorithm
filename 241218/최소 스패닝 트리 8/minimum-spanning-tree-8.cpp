a3ecabdc66acb151fbc20149e6c59302274e63cd
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-12-18 16:55:25 +0900

    Title: 최소 스패닝 트리 8, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241218/최소 스패닝 트리 8/minimum-spanning-tree-8.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+#define MAX_N 500
+using namespace std;
+
+int n,m;
+priority_queue<pair<int,int>>pq;
+bool visited[MAX_N+1];
+int dist[MAX_N+1];
+vector<pair<int,int>>graph[MAX_N+1];
+
+int main() {
+    int ans=0;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int x,y,w;
+        cin>>x>>y>>w;
+        graph[x].push_back(make_pair(w,y));
+        graph[y].push_back(make_pair(w,x));
+    }
+    for(int i=1;i<=n;i++){
+        dist[i]=(int)1e9;
+    }
+    pq.push(make_pair(-0,1));
+    dist[1]=0;
+    while(!pq.empty()){
+        int w,x;
+        tie(w,x)=pq.top();
+        pq.pop();
+        if(visited[x])continue;
+        visited[x]=true;
+        
+        w=-w;
+        ans+=w;
+        for(int i=0;i<(int)graph[x].size();i++){
+            int len,y;
+            tie(len,y)=graph[x][i];
+            if(dist[y]>=len+dist[x]){
+                dist[y]=len+dist[x];
+                pq.push(make_pair(-len,y));
+            }
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
