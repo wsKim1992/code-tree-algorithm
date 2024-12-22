0a5e83d2b70bc6497983f24ac1a3cb5e1070ea38
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-12-22 15:08:04 +0900

    Title: 트리 복원하기, Time: 110ms, Memory: 5MB, Status: Passed - CodeTree

--- 241222/트리 복원하기/restore-tree.cpp
+++ 241222/트리 복원하기/restore-tree.cpp
@@ -13,6 +13,7 @@ int dist[MAX_N+1];
 bool visited[MAX_N+1];
 priority_queue<tuple<int,int,int>>pq;
 int M[MAX_N+1][MAX_N+1];
+vector<tuple<int,int,int>>ans;
 int main() {
     cin>>n;
     for(int i=1;i<=n;i++){
@@ -27,28 +28,34 @@ int main() {
         }   
     }
     dist[1]=0;
-    pq.push(make_tuple(-0,1,MAX_N));
+    pq.push(make_tuple(-0,-1,MAX_N));
     while(!pq.empty()){
         int w,x,y;
         tie(w,x,y)=pq.top();
         pq.pop();
+        x=-x;
         if(visited[x])continue;
         visited[x]=true;
         w=-w;
         if(y!=MAX_N){
             y=-y;
-            if(x<=y)cout<<x<<" "<<y<<" ";
-            else cout<<y<<" "<<x<<" ";
-            cout<<w<<endl;
+            if(x<=y)ans.push_back(make_tuple(x,y,w));
+            else ans.push_back(make_tuple(y,x,w));
         }
         for(int i=0;i<(int)graph[x].size();i++){
             int w1,y;
             tie(w1,y)=graph[x][i];
             if(dist[y]>w1){
                 dist[y]=w1;
-                pq.push(make_tuple(-w1,y,-x));
+                pq.push(make_tuple(-w1,-y,-x));
             }
         }
+    }
+    sort(ans.begin(),ans.end());
+    for(auto iter=ans.begin();iter!=ans.end();iter++){
+        int x,y,w;
+        tie(x,y,w)=(*iter);
+        cout<<x<<" "<<y<<" "<<w<<endl;
     }
     return 0;
 }
