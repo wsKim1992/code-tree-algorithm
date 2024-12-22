7da5c61d193fa5e223a6c2a990d1b87a84c33d82
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-12-22 19:11:48 +0900

    Title: 조립 2, Time: 69ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 241222/조립 2/assembly-2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#define MAX_N 100000
+
+using namespace std;
+
+int n,m,nodes;
+vector<int>graph[MAX_N+1];
+queue<int>q;
+priority_queue<int>ans;
+int inDegree[MAX_N+1];
+
+int main() {
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int x,k;
+        cin>>x>>k;
+        while(k--){
+            int y;
+            cin>>y;
+            graph[y].push_back(x);
+            inDegree[x]++;
+        }
+    }
+    cin>>nodes;
+    for(int i=0;i<nodes;i++){
+        int y;
+        cin>>y;
+        if(inDegree[y]==0){
+            ans.push(-y);
+            q.push(y);
+        }
+    }
+    while(!q.empty()){
+        int x= q.front();
+        q.pop();
+        for(int i=0;i<(int)graph[x].size();i++){
+            int y = graph[x][i];
+            inDegree[y]--;
+            if(inDegree[y]==0){
+                q.push(y);
+                ans.push(-y);
+            }
+        }
+    }
+    cout<<ans.size()<<endl;
+    while(!ans.empty()){
+        cout<<-ans.top()<<" ";
+        ans.pop();
+    }
+    return 0;
+}
