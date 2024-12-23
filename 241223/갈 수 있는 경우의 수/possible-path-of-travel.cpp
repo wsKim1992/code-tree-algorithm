c65f27c8d775eb772736c862522dd34209ae75c7
wsKim1992 <42395357+wsKim1992@users.noreply.github.com>
2024-12-23 16:35:50 +0900

    Title: 갈 수 있는 경우의 수, Time: 44ms, Memory: 3MB, Status: Wrong Answer - CodeTree

---
+++ 241223/갈 수 있는 경우의 수/possible-path-of-travel.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <tuple>
+#include<math.h>
+#define MAX_N 100000
+using namespace std;
+
+int n,m;
+vector<int>edges[MAX_N+1];
+queue<int>q;
+long dist[MAX_N+1];
+int inDegree[MAX_N+1];
+
+int main() {
+    cin>>n>>m;
+    long ans = 0;
+    for(int i=0;i<m;i++){
+        int x,y;
+        cin>>x>>y;
+        edges[x].push_back(y);
+        inDegree[y]++;
+    }
+    for(int i=1;i<=n;i++){
+        if(inDegree[i]==0){
+            dist[i]=1;
+            q.push(i);
+        }
+    }
+    while(!q.empty()){
+        int x= q.front();
+        q.pop();
+        for(int i=0;i<(int)edges[x].size();i++){
+            int y = edges[x][i];
+            dist[y]+=dist[x];
+            inDegree[y]-=1;
+            if(!inDegree[y]){
+                q.push(y);
+            }
+        }
+    }
+    for(int i=1;i<=n;i++){
+        ans = max(ans,dist[i]);
+    }
+    long operends = pow(10,9)+7;
+    cout<<ans%operends<<endl;
+    return 0;
+}
